2cb3deedc910ed0c49565fdbb6484e7aa9bbe345
kalyangitnew <venkatakalyan9704@gmail.com>
2022-01-04 15:17:14 +0530

    added she-bang

---
+++ 01-Shell.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# #! is called as she-bang.It denotes the path of interpreter and also ensures the script executes with that interpreter.if we execute the script with ./script instead of bash .script.sh
+
+# any line starting with "#" is called as comment and this lines will be ignored during the execution time.
+ls
+ls -ltr
+
+
+
+
+
