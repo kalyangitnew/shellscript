c0fbefb31b89c7ff39ee4b3b4996ec76bb91babc
kalyangitnew <venkatakalyan9704@gmail.com>
2022-01-04 15:38:33 +0530

    print.sh file is added and mentioned it uses

---
+++ 02-print.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# to print some info on the screen we will echo command and prints.sh
+# here we will goint to use echo command in our practice
+# syntax is below mentioned
+# echo message -- just it will print message
+
+echo hello world this is kalyan
+echo welcome
+
