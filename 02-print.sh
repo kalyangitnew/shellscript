eb2e8e25cd0145ae209686b050b74f13e7fde125
kalyangitnew <venkatakalyan9704@gmail.com>
2022-01-04 16:09:23 +0530

    final print.sh with esc sequences and colours concept included

--- 02-print.sh
+++ 02-print.sh
@@ -11,8 +11,20 @@ echo welcome
 # printing information is very useful while running to tell user what is happening and what is executing
 
 # to get attention from users we will use some commands called "ESC sequences"
-# \n --- new line  and \e ---- new tab
+# \n --- new line  and \e ---- new tab \t for tab spaces
 # syntax : echo -e "message1\n message2"
 echo -e "kalyan \n hello"
 # for using any esc sequences we will use "-e" and invereted commas for in line.
 # we can run two comments in terminal by ";" example git pull ; bash 02-print.sh
+# COLOURED output
+# syntax: echo -e "\e[COLmMESGAGE" , IN this col is colour code \e is to execute in colour and "m" is added for executing.
+##COLORS  #CODE
+#RED       31
+#GREEN     32
+#YELLOW    33
+#BLUE      34
+#MAGENTA   35
+#CYAN      36
+
+echo -e "\e[34mText in blue colour"
+
