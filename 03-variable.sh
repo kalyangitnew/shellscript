de5e6eecb44a82bdf3be415290cc68b6715b8d9f
kalyangitnew <venkatakalyan9704@gmail.com>
2022-01-04 17:01:24 +0530

    Variables files

---
+++ 03-variable.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+## if we assign a name to se of data then it is called as variable
+# SYNTAX : VAR=DATA
+
+# Number
+a=100
+# string
+b=abc
+
+# In bash shell there are no different data types.it considers everything as 'string"
+
+# access the data in shell using "$" charectes prefixing the variable name
+
+echo value of a = $a
+echo value of b = $b
