cb28fac2a88b29a3e444846d1b9bfc5d3bc42598
kalyangitnew <venkatakalyan9704@gmail.com>
2022-01-04 19:25:57 +0530

    read file

--- 04-read.sh
+++ 04-read.sh
@@ -4,3 +4,10 @@
 read -p 'ENTER YOUR NAME: ' name
 read -p 'ENTER YOUR AGE: ' age
 echo -e "your name = $name\nyour age = $age"
+
+# above approach used when the script executed manually.so when automation setup is will not work
+# for that before execution we will store store some inputs and those inputs will be loaded by script
+# special variables are &0-&n,&*,&@.&#
+# $0 - script name
+#
+
