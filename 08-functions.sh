6e4cbf8465b9e8ccfb90893a9143d7d4f09fd11b
kalyangitnew <venkatakalyan9704@gmail.com>
2022-01-05 16:28:04 +0530

    function

--- 08-functions.sh
+++ 08-functions.sh
@@ -25,9 +25,9 @@ foo
 function xyz() {
   echo i am function of xyz
   b=20
-  echo c variable is taken from proagram to function=$c
+  echo variable is taken from proagram to function = $c
 }
 ## main programme line is before for above function
+c=23
 xyz
 echo b in main programme from it function=$b
-c=32
