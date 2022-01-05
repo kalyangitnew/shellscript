90c530f891f5732d8c2f062ac1c8c8bebbb6b580
kalyangitnew <venkatakalyan9704@gmail.com>
2022-01-05 16:10:08 +0530

    function

---
+++ 08-functions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# if we assign a name to some set of commands then it is called function.
+# functions are used to keep code "DRY" (MEANS NO NEED TO REPEAT)
+# FUNCTIONS are also used to group the commands logically
+ # functions should always declare before using like variable
+ # thats why functions are find at starting of the function
+  # example beloe
+
+function abc() {
+  echi i am function of abc
+}
+
