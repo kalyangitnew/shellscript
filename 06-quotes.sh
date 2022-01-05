f0878a7b6083187aedee6c91d2c171b491e54a29
kalyangitnew <venkatakalyan9704@gmail.com>
2022-01-05 15:40:57 +0530

    quotes file

--- 06-quotes.sh
+++ 06-quotes.sh
@@ -9,6 +9,9 @@
 
 
 # redirectors
-# < and >
+# < and > these are STDIN & STDOUT
 # > is output will go to that file , open any folder do ls >/tmp/out all files in opened folder copied to /tmp/out
 # < input is taking to that file , example mongo < /tmp/mongo if we load any commands in mongo file all will be executed mongo command(takes input)
+# " >> " it is used for appending symbol means everytime new line will be added with previous  example : date >>/tmp/date
+# > for std out ,,   &> for stderr.
+# if we dnt want any error to save    "  &>dev/null  "
