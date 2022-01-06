c2d3801f4e69e57fee862e7a30e9392824775057
kalyangitnew <venkatakalyan9704@gmail.com>
2022-01-06 22:13:07 +0530

    instance creating

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -2,10 +2,11 @@
 COUNT=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null  | wc -l)
 
 if [ $COUNT -eq 0 ]; then
-  aws ec2 run-instances --image-id ami-0760b951ddb0c20c9 --instance-type t3.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
+  aws ec2 run-instances --image-id ami-0760b951ddb0c20c9 --instance-type t3.micro --security-group-ids sg-0db1f455d56fb942d --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
 else
   echo "Instance which you are creating already exists"
 fi
 
 # in above we have some count function and there we have stored the private ip address value of instance named "something which is given in CLI"
- # If the named instances private address value after removing null statements is zero we have written a condition saying to create if not create new instance with the name given in CLI
+# If the named instances private address value after removing null statements is zero we have written a condition saying to create if not create new instance with the name given in CLI
+# we have choosen ip address because as instance terminated immediately ip address will gone
