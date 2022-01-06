762f5d44291041417562e3169a39f43ea40ef6bb
kalyangitnew <venkatakalyan9704@gmail.com>
2022-01-06 18:18:52 +0530

    instance creating

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-aws ec2 run-instances --image-id ami-0760b951ddb0c20c9 --instance-type t3.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]"
+aws ec2 run-instances --image-id ami-0760b951ddb0c20c9 --instance-type t3.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$1}]" | jq
