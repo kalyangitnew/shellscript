fe356ad7fff087f9b1015873052e3d54d804d127
kalyangitnew <venkatakalyan9704@gmail.com>
2022-01-06 15:32:59 +0530

    if else conditions

--- 09-if.sh
+++ 09-if.sh
@@ -6,6 +6,8 @@
 # 1. string comparison 2 . number comparison 3.file comparison.
 # 1.string
 # the operators are = , == , != , -z
+# 2. number
+# the operators are -eq , -ne , -gt , -ge , -lt , -gt
 read -p 'enter username: ' username
 
 if [ "$username" == "root" ]; then
@@ -19,3 +21,8 @@ if [ $UID -eq 0 ]; then
 else
   echo "your are not root user"
 fi
+
+# first condition is of type "string"
+# second if type is of type "number"
+# we can search for file existing with file comparison
+# syntax for file is [ -f file ] or [ ! -f file ]
