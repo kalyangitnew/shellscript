caefbdebd8712f75e3309d98fcb7c1280cbb167e
kalyangitnew <venkatakalyan9704@gmail.com>
2022-01-07 00:17:55 +0530

    mongod automation

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -31,3 +31,17 @@ Print "restarted after changing DNS"
 systemctl restart mongo &>>$LOG
 stat $?
 
+# Download the schema and load it.
+
+Print "Download the schema file"
+curl -s -L -o /tmp/mongodb.zip "https://github.com/roboshop-devops-project/mongodb/archive/main.zip" &>>$LOG
+stat $?
+
+Print "unzip the schema"
+unzip -o -d /tmp /tmp/mongodb.zip &>>$LOG
+stat $?
+
+Print "loading schema"
+cd /tmp/mongodb-main
+mongo < catalogue.js &>>$LOG
+mongo < users.js &>>$LOG
