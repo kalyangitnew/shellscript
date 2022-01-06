d67458c463cef4b3bd4d2549c1a1dc4352b091a2
kalyangitnew <venkatakalyan9704@gmail.com>
2022-01-06 15:11:15 +0530

    if else conditions

--- 09-if.sh
+++ 09-if.sh
@@ -1,2 +1,5 @@
 # there are three conditions
 # 1.if statement 2.tf else statement 3. else if statement(multi-tier)
+# to make decisions fro condtions we will write some "expressions"
+# expressions are also three types
+# 1. string comparison 2 . numeber comparison 3.file comparison.
