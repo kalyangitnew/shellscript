213afa1ca0508627e0485ca18381a69e98742af7
kalyangitnew <venkatakalyan9704@gmail.com>
2022-01-06 15:18:55 +0530

    if else conditions

--- 09-if.sh
+++ 09-if.sh
@@ -1,5 +1,14 @@
+#!/bin/bash
 # there are three conditions
 # 1.if statement 2.tf else statement 3. else if statement(multi-tier)
-# to make decisions fro condtions we will write some "expressions"
+# to make decisions fro conditions we will write some "expressions"
 # expressions are also three types
-# 1. string comparison 2 . numeber comparison 3.file comparison.
+# 1. string comparison 2 . number comparison 3.file comparison.
+# 1.string
+# the operators are = , == , != , -z
+read -p 'enter username: ' username
+if [ "$username == root" ]; then
+  echo "you are root user"
+else
+  echo "you are not root user"
+fi
