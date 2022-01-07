ec54be09ccca78dcfac668292c7442d3822375c1
kalyangitnew <venkatakalyan9704@gmail.com>
2022-01-07 16:17:19 +0530

    mongod automation

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -66,10 +66,11 @@ Print "system restarting"
 systemctl daemon-reload &>>$LOG && systemctl start catalogue &>>$LOG && systemctl enable catalogue &>>$LOG
 stat $?
 
+sleep 5
+
 Print "checking DB connection from app"
 STAT=$(curl -s localhost:8080/health | jq .mongo)
-echo status = $STAT
-if ("$STAT" = "true"); then
+if [ "$STAT" = "true" ]; then
   Stat 0
 else
   Stat 1
