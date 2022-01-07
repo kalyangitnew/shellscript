e89d961326e6f74b9c512d8b48adfa9f8a9342dd
kalyangitnew <venkatakalyan9704@gmail.com>
2022-01-07 13:27:12 +0530

    mongod automation

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -18,3 +18,5 @@ IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reser
 # xargs is to remove the double quotation
 sed -e "s/DNSNAME/$1.roboshop.internal/" -e "s/IPADDRESS/${IP}/" record.json >/tmp/record.json
 # we are saving the ip and DNS in tmp file instead of output
+
+aws route53 change-resource-record-sets --hosted-zone-id Z01711311HI659T5X3F6I --change-batch file:///tmp/record.json | jq
