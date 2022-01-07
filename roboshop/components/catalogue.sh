b0ba150947a9877cfd459cff494d4848d03b882d
kalyangitnew <venkatakalyan9704@gmail.com>
2022-01-07 17:17:21 +0530

    mongod automation

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -70,8 +70,8 @@ sleep 5
 
 Print "checking DB connection from app"
 STAT=$(curl -s localhost:8080/health | jq .mongo)
-if [[ "$STAT" == "true" ]]; then
-  STAT 0
+if [ "$STAT" == "true" ]; then
+  Stat 0
 else
-  STAT 1
+  Stat 1
 fi
