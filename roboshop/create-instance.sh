5a06ef92bd5a0f729437d232e5646c11b2d4d65c
kalyangitnew <venkatakalyan9704@gmail.com>
2022-01-07 13:12:04 +0530

    mongod automation

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -15,5 +15,5 @@ fi
 
 
 IP=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$1" | jq ".Reservations[].Instances[].PrivateIpAddress" | grep -v null )
-sed -e "s/DNSNAME/$1.roboshop.internal" -e "s/IPADDRESS/${IP}/" record.json >/tmp/record.json
+sed -e "s/DNSNAME/$1.roboshop.internal" -e "s/IPADDRESS/${IP}" record.json >/tmp/record.json
 # we are saving the ip and DNS in tmp file instead of output
