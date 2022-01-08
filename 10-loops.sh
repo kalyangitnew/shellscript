39ddd292248b495981f34b3c05cc6c83b76960fd
kalyangitnew <venkatakalyan9704@gmail.com>
2022-01-08 16:26:45 +0530

    while loop updated

--- 10-loops.sh
+++ 10-loops.sh
@@ -13,3 +13,14 @@ while [ $a -gt 0 ]; do
 done
 # it may be infinite we need to set up some finite see common.sh or other components files for use
 #
+b=10
+for b in 1 2 3 4 5 6 7 8 9 10
+do
+    # if b is equal to 5 break the loop
+    if [ $b == 5 ]
+    then
+        break
+    fi
+    # Print the value
+    echo "Iteration no $b"
+done
