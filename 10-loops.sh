596fd819358d8394ebda6a3f5fb4fe6b9a1770b9
kalyangitnew <venkatakalyan9704@gmail.com>
2022-01-08 16:28:26 +0530

    while loop updated

--- 10-loops.sh
+++ 10-loops.sh
@@ -5,6 +5,21 @@
 # for reverse of while is until and reverse for "for" is select
 
 # while uses for expression in functions
+
+b=10
+for b in 1 2 3 4 5 6 7 8 9 10
+do
+    # if b is equal to 5 break the loop
+    if [ $b == 1 ]
+    then
+        break
+    fi
+    # Print the value
+    echo "Iteration no $b"
+done
+
+exit
+
 a=10
 while [ $a -gt 0 ]; do
   echo while Loops
@@ -12,15 +27,3 @@ while [ $a -gt 0 ]; do
   a=$((a-1))
 done
 # it may be infinite we need to set up some finite see common.sh or other components files for use
-#
-b=10
-for b in 1 2 3 4 5 6 7 8 9 10
-do
-    # if b is equal to 5 break the loop
-    if [ $b == 5 ]
-    then
-        break
-    fi
-    # Print the value
-    echo "Iteration no $b"
-done
